6514880ce36e3e4785ede83acf20b5f7f24aaad0
Getkart-Nonish <nonish.jain@getkart.com>
2024-02-18 22:41:43 +0530

    chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/portfolio",
-  output: "export",  // <=== enables static exports
-  images: { unoptimized: true }
 };
 
 export default nextConfig;
