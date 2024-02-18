f3e958f45ac9e830a7b34668bc713083fca184e1
Getkart-Nonish <nonish.jain@getkart.com>
2024-02-18 17:01:40 +0530

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/portfolio",
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  // images: { unoptimized: true }
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
